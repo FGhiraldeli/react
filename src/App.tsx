82abe94f62866ff00ccd139159f75671843fad1c
Flávio Ghiraldeli <ghiraldeli26@gmail.com>
2025-11-12 18:32:15 -0300

    Adding alert with Bootstrap

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,23 @@
 /** @format */
 
-import ListGroup from "./components/ListGroup";
+import Alert from "./components/Alert";
+
+// import ListGroup from "./components/ListGroup";
 
 function App() {
-  let items = ["New York", "San Francisco", "Tokyo", "London", "Brazil"];
+  //   let items = ["New York", "San Francisco", "Tokyo", "London", "Brazil"];
+
+  //   const handleSelectItem = (item: string) => {
+  //     console.log(item);
+  //   };
   return (
     <div>
-      <ListGroup items={items} heading='Cities' />
+      <Alert />
+      {/* <ListGroup
+        items={items}
+        heading='Cities'
+        onSelectItem={handleSelectItem}
+      /> */}
     </div>
   );
 }
---
+++ src/components/Alert.tsx
@@ -0,0 +1,7 @@
+/** @format */
+
+const Alert = () => {
+  return <div className='alert alert-primary'>Alert</div>;
+};
+
+export default Alert;
